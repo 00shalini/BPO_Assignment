08114e339cf7793fc4fd072793b2eea35d381375
00shalini <shalini07061997@gmsail.com>
2022-06-29 00:05:57 +0530

    first question

--- products.js
+++ products.js
@@ -27,5 +27,18 @@ const listOfProducts = [
 ];
 
 var obj = {};
+var p_count = 0;
+var value = 1;
 
-for (var i = 0; i < listOfProducts.length; i++) {}
+for (var i = 0; i < listOfProducts.length; i++) {
+    
+  if ( obj[listOfProducts[i].productName] == undefined){
+    obj[listOfProducts[i].productName] = value;
+  } else {
+    obj[listOfProducts[i].productName] == listOfProducts[i].productName;
+    value = obj[listOfProducts[i].productName];
+    obj[listOfProducts[i].productName] = value+1;
+
+  }
+}
+console.log(obj);
